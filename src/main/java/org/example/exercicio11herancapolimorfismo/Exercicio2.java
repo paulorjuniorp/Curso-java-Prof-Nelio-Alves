016fe61f3226e3a3410e5572f9a9485d87b71a59
paulorjuniorp <paulorjuniorp@gmail.com>
2023-05-20 19:02:27 -0300

    Resolução do exercício proposto sobre com classes abstratas e polimorfismo.

--- src/main/java/org/example/exercicio11herancapolimorfismo/Exercicio2.java
+++ src/main/java/org/example/exercicio11herancapolimorfismo/Exercicio2.java
@@ -1,7 +1,69 @@
 package org.example.exercicio11herancapolimorfismo;
 
+import org.example.exercicio11herancapolimorfismo.entities.Contribuinte;
+import org.example.exercicio11herancapolimorfismo.entities.PessoaFisica;
+import org.example.exercicio11herancapolimorfismo.entities.PessoaJuridica;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Exercicio2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Contribuinte contribuinte;
+        List<Contribuinte> contribuintes = new ArrayList<>();
 
+        System.out.print("Digite a quantidade de contribuites: ");
+        int entrada = scanner.nextInt();
+
+        for (int i = 1; i <= entrada; i++){
+            System.out.printf("Dados do contribuinte #%d%n",i);
+            System.out.print("Pessoa física ou juridica (f/j)? ");
+            scanner.nextLine();
+            char opcao = scanner.nextLine().charAt(0);
+            System.out.print("Nome: ");
+            String nome = scanner.nextLine();
+
+            System.out.print("Renda anual: ");
+            double rendaAnual = scanner.nextDouble();
+
+            switch (opcao){
+                case 'f':
+                    scanner.nextLine();
+                    System.out.print("Gastos com saúde: ");
+                    double gastoComSaude = scanner.nextDouble();
+
+                    contribuinte = new PessoaFisica(nome,rendaAnual,gastoComSaude);
+                    contribuintes.add(contribuinte);
+                    break;
+                case 'j':
+                    System.out.print("Número de funcionários: ");
+                    int numeroDeFuncionarios = scanner.nextInt();
+
+                    contribuinte = new PessoaJuridica(nome,rendaAnual,numeroDeFuncionarios);
+                    contribuintes.add(contribuinte);
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        }
+
+        System.out.println();
+        System.out.println("IMPOSTOS PAGOS: ");
+        for (Contribuinte contribuinteImpressao : contribuintes) {
+            System.out.println(contribuinteImpressao.getNome() + String.format(": R$ %.2f",contribuinteImpressao.imposto()));
+        }
+
+        double somaTaxa = 0;
+        for (Contribuinte contribuinteSomaTaxa : contribuintes) {
+            somaTaxa += contribuinteSomaTaxa.imposto();
+        }
+
+        System.out.println();
+        System.out.println("Total de impostos: R$ " + String.format("%.2f",somaTaxa));
+
+        scanner.close();
     }
 }
