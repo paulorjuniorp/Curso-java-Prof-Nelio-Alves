ba20e4bda94232f6cb073993af841b0a1d05e78c
paulorjuniorp <paulorjuniorp@gmail.com>
2023-05-23 06:42:12 -0300

    Exemplo com a resolução de exception de forma ruim

--- src/main/java/org/example/exercicio12tratamentoexcecoes/exemplos/application/Exemplo2.java
+++ src/main/java/org/example/exercicio12tratamentoexcecoes/exemplos/application/Exemplo2.java
@@ -7,17 +7,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Scanner;
 
-/**
- * Fazer um programa para ler os dados de uma reserva de hotel (número do quarto, data de entrada e data
- * de saída) e mostrar os dados da reserva, inclusive sua duração em dias. Em seguida, ler novas datas de entrada
- * e saída, atualizar a reserva, e mostrar novamente a reserva com os dados atualizados. O programa não deve aceitar
- * dados inválidos para a reserva, conforme as seguintes regras:
- *      -Alterações de reserva só podem ocorrer para datas futuras
- *      -A data de saída deve ser maior que a data de entrada
- * */
-
-//Exemplo de uma implementação muito ruim: erro de delegação pois a lógica de validação está na classe main
-public class Exemplo1 {
+//resolução ruim
+public class Exemplo2 {
     public static void main(String[] args) throws ParseException {
         Scanner scanner = new Scanner(System.in);
         SimpleDateFormat formatoData = new SimpleDateFormat("dd/MM/yyyy");
@@ -28,6 +19,7 @@ public class Exemplo1 {
         Date checkIn = formatoData.parse(scanner.next());
         System.out.print("Check-out date (dd/MM/yyyy) ");
         Date checkOut = formatoData.parse(scanner.next());
+        // essa validação deveria ser feita no construtor
         if (!checkOut.after(checkIn)){
             System.out.println("Error in reservation: Check-out date must be after check-in date");
         } else {
@@ -41,16 +33,13 @@ public class Exemplo1 {
             System.out.print("Check-out date (dd/MM/yyyy): ");
             checkOut = formatoData.parse(scanner.next());
 
-            Date now = new Date();
-            if (checkIn.before(now) || checkOut.before(now)){
-                System.out.println("Error in reservation: Reservation dates for update must be future dates");
-            } else if (!checkOut.after(checkIn)){
-                System.out.println("Error in reservation: Check-out date must be after check-in date");
+
+            String error = reservation.updateDates(checkIn, checkOut);
+            if (error != null){
+                System.out.println(error);
             } else {
-                reservation.updateDates(checkIn,checkOut);
                 System.out.println("Reservation: " + reservation);
             }
-
         }
 
         scanner.close();
