a14d5bd8677edacd4f596667b3350d63125a3818
paulorjuniorp <paulorjuniorp@gmail.com>
2023-06-15 07:00:47 -0300

    Primeiro exemplo do módulo de interfaces

---
+++ src/main/java/org/example/exercicio14interfaces/exemplos/Exemplo1.java
@@ -0,0 +1,36 @@
+package org.example.exercicio14interfaces.exemplos;
+
+/**
+ * Uma locadora brasileira de carros cobra um valor por hora para locações de até 12 horas. Porém, se a
+ * duração da locação ultrapassar 12 horas, a locação será cobrada com base em um valor diário. Além do valor
+ * da locação, é acrescido no preço o valor do imposto conforme regras do país que, no caso do Brasil, é 20%
+ * para valores até 100.00, ou 15% para valores acima de 100.00. Fazer um programa que lê os dados da locação
+ * (modelo do carro, instante inicial e final da locação), bem como o valor por hora e o valor diário de locação.
+ * O programa deve então gerar a nota de pagamaneto (contendo o valor da locação, valor do imposto e valor
+ * total do pagamento) e informar os dados na tela. Veja os exemplos.
+ *
+ * Entre com os dados do aluguel
+ * Modelo do carro: Civic
+ * Retirada (dd/MM/yyyy hh:mm): 25/06/2018 10:30
+ * Retorno (dd/MM/yyyy hh:mm): 25/06/2018 14:40
+ * Entre com o preço por hora: 10.00
+ * Entre com o preço por dia: 130.00
+ * FATURA:
+ * Pagamento basico: 50.00
+ * Imposto: 10.00
+ * Pagamento total: 60.00
+ *
+ * Entre com os dados do aluguel
+ * Modelo do carro: Civic
+ * Retirada (dd/MM/yyyy hh:mm): 25/06/2018 10:30
+ * Retorno (dd/MM/yyyy hh:mm): 27/06/2018 11:40
+ * Entre com o preço por hora: 10.00
+ * Entre com o preço por dia: 130.00
+ * FATURA:
+ * Pagamento basico: 390.00
+ * Imposto: 58.50
+ * Pagamento total: 448.50
+ *
+ * */
+public class Exemplo1 {
+}
