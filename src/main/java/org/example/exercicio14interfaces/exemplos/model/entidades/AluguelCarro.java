195843b914b798796f97f6a81641a306cefb30b3
paulorjuniorp <paulorjuniorp@gmail.com>
2023-06-15 07:22:14 -0300

    Estrutura da parte 1 do exemplo

---
+++ src/main/java/org/example/exercicio14interfaces/exemplos/model/entidades/AluguelCarro.java
@@ -0,0 +1,49 @@
+package org.example.exercicio14interfaces.exemplos.model.entidades;
+
+import java.time.LocalDateTime;
+
+public class AluguelCarro {
+    private LocalDateTime inicio;
+    private LocalDateTime fim;
+    private Veiculo veiculo;
+    private Fatura fatura;
+
+    public AluguelCarro(LocalDateTime inicio, LocalDateTime fim, Veiculo veiculo, Fatura fatura) {
+        this.inicio = inicio;
+        this.fim = fim;
+        this.veiculo = veiculo;
+        this.fatura = fatura;
+    }
+
+    public LocalDateTime getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(LocalDateTime inicio) {
+        this.inicio = inicio;
+    }
+
+    public LocalDateTime getFim() {
+        return fim;
+    }
+
+    public void setFim(LocalDateTime fim) {
+        this.fim = fim;
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public Fatura getFatura() {
+        return fatura;
+    }
+
+    public void setFatura(Fatura fatura) {
+        this.fatura = fatura;
+    }
+}
