1335071e69a1cf21b4d09b2a0b358216c80c3a30
paulorjuniorp <paulorjuniorp@gmail.com>
2023-06-15 07:21:04 -0300

    Estrutura da parte 1 do exemplo

---
+++ src/main/java/org/example/exercicio14interfaces/exemplos/model/entidades/Fatura.java
@@ -0,0 +1,31 @@
+package org.example.exercicio14interfaces.exemplos.model.entidades;
+
+public class Fatura {
+    private Double pagamentoBasico;
+    private Double imposto;
+
+    public Fatura(Double pagamentoBasico, Double imposto) {
+        this.pagamentoBasico = pagamentoBasico;
+        this.imposto = imposto;
+    }
+
+    public Double getPagamentoBasico() {
+        return pagamentoBasico;
+    }
+
+    public void setPagamentoBasico(Double pagamentoBasico) {
+        this.pagamentoBasico = pagamentoBasico;
+    }
+
+    public Double getImposto() {
+        return imposto;
+    }
+
+    public void setImposto(Double imposto) {
+        this.imposto = imposto;
+    }
+
+    public double getPagamentoTotal(){
+        return getPagamentoBasico() - getImposto();
+    }
+}
