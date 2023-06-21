819abeb31a3cb636bec00aabe9d7f3cbd0671249
paulorjuniorp <paulorjuniorp@gmail.com>
2023-06-21 07:00:04 -0300

    ContratoService

---
+++ src/main/java/org/example/exercicio14interfaces/exercicios/model/services/ContratoService.java
@@ -0,0 +1,9 @@
+package org.example.exercicio14interfaces.exercicios.model.services;
+
+import org.example.exercicio14interfaces.exercicios.model.entities.Contrato;
+
+public class ContratoService {
+    public void processoContrato(Contrato contrato, Integer parcelas){
+
+    }
+}
