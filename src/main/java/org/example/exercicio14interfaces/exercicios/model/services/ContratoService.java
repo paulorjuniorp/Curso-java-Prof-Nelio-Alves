27be87048c157e1b43df666ebfc75ec204389ac4
paulorjuniorp <paulorjuniorp@gmail.com>
2023-06-22 06:55:11 -0300

    Camada de serviço

--- src/main/java/org/example/exercicio14interfaces/exercicios/model/services/ContratoService.java
+++ src/main/java/org/example/exercicio14interfaces/exercicios/model/services/ContratoService.java
@@ -3,6 +3,8 @@ package org.example.exercicio14interfaces.exercicios.model.services;
 import org.example.exercicio14interfaces.exercicios.model.entities.Contrato;
 import org.example.exercicio14interfaces.exercicios.model.entities.Parcela;
 
+import java.time.LocalDate;
+
 public class ContratoService {
     private PagamentoOnlineService pagamentoOnlineService;
 
@@ -10,7 +12,19 @@ public class ContratoService {
         this.pagamentoOnlineService = pagamentoOnlineService;
     }
 
-    public void processoContrato(Contrato contrato, Integer parcelas){
-        contrato.getParcelas().add(new Parcela());
+    public void processoContrato(Contrato contrato, int meses){
+
+        double parcelaBasica = contrato.getValorContrato() / meses;
+
+        for (int i = 1; i <= meses; i++){
+            LocalDate dataVencimento = contrato.getDataContrato().plusMonths(i);
+
+            double juro = pagamentoOnlineService.juros(parcelaBasica, i);
+            double taxa = pagamentoOnlineService.taxaPagamento(parcelaBasica + juro);
+            double parcela = parcelaBasica + juro + taxa;
+
+            contrato.getParcelas().add(new Parcela(parcela, dataVencimento));
+        }
+
     }
 }
