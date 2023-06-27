15340a6025f2812ee5def0a47499f7724d341043
paulorjuniorp <paulorjuniorp@gmail.com>
2023-06-27 06:51:35 -0300

    Exemplo com generics

---
+++ src/main/java/org/example/exercicio15genericsetmap/exemplos/application/Exemplo1.java
@@ -0,0 +1,9 @@
+package org.example.exercicio15genericsetmap.exemplos.application;
+
+/**
+ * Deseja-se fazer um programa que leia uma quantidade N, e depois N números inteiros. Ao final, imprima esses
+ * números de forma organizada conforme exemplo. Em seguida, informar qual foi o primeiro valor informado.
+ * */
+public class Exemplo1 {
+
+}
---
+++ src/main/java/org/example/exercicio15genericsetmap/exemplos/model/service/PrintService.java
@@ -0,0 +1,18 @@
+package org.example.exercicio15genericsetmap.exemplos.model.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintService {
+
+    List<Integer> list = new ArrayList<>();
+    public void addValue(int value){
+        list.add(value);
+    }
+    public int first(){
+        return list.get(0);
+    }
+    public void printValue(){
+        for (Integer integer : list) System.out.println(integer);
+    }
+}
