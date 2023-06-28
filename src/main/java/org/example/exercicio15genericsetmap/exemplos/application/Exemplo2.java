e979b46a31023bb9af535cfe51c13c7f51eabb6a
paulorjuniorp <paulorjuniorp@gmail.com>
2023-06-28 07:09:40 -0300

    Exemplo com generics delimitados

--- src/main/java/org/example/exercicio15genericsetmap/exemplos/application/Exemplo2.java
+++ src/main/java/org/example/exercicio15genericsetmap/exemplos/application/Exemplo2.java
@@ -1,5 +1,13 @@
 package org.example.exercicio15genericsetmap.exemplos.application;
 
+import org.example.exercicio15genericsetmap.exemplos.model.service.CalculationService;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Uma empresa de consultoria deseja avaliar a performance de produtos, funcionários, dentre outras coisas.
  * Um dos cálculos que ela precisa é encontrar o maior dentre um conjunto de elementos. Fazer um programa
@@ -8,4 +16,22 @@ package org.example.exercicio15genericsetmap.exemplos.application;
  *
  * */
 public class Exemplo2 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        String path = "C:\\temp\\in.txt";
+
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
+            String line = bufferedReader.readLine();
+            while (line != null){
+                list.add(Integer.parseInt(line));
+                line = bufferedReader.readLine();
+            }
+
+//            Integer x = CalculationService.max(list);
+//            System.out.println("Max: ");
+//            System.out.println(x);
+        } catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
