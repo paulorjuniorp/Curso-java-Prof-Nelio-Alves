c646fd355e60856cd3eb1e173c081ddb43959b5e
paulorjuniorp <paulorjuniorp@gmail.com>
2023-06-28 07:03:14 -0300

    Exemplo com generics delimitados

---
+++ src/main/java/org/example/exercicio15genericsetmap/exemplos/application/Exemplo2.java
@@ -0,0 +1,11 @@
+package org.example.exercicio15genericsetmap.exemplos.application;
+
+/**
+ * Uma empresa de consultoria deseja avaliar a performance de produtos, funcionários, dentre outras coisas.
+ * Um dos cálculos que ela precisa é encontrar o maior dentre um conjunto de elementos. Fazer um programa
+ * que leia um conjunto de produtos a partir de um arquivo, conforme exemplo, e depois mostre o mais caro
+ * deles.
+ *
+ * */
+public class Exemplo2 {
+}
