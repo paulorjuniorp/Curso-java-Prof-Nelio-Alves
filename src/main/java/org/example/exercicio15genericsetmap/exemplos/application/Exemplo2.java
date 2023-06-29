24ebfaabcc978ddddc0580e0e413210321bb97c7
paulorjuniorp <paulorjuniorp@gmail.com>
2023-06-29 07:06:00 -0300

    Exemplo com generics delimitados

--- src/main/java/org/example/exercicio15genericsetmap/exemplos/application/Exemplo2.java
+++ src/main/java/org/example/exercicio15genericsetmap/exemplos/application/Exemplo2.java
@@ -1,5 +1,6 @@
 package org.example.exercicio15genericsetmap.exemplos.application;
 
+import org.example.exercicio15genericsetmap.exemplos.model.entities.Product;
 import org.example.exercicio15genericsetmap.exemplos.model.service.CalculationService;
 
 import java.io.BufferedReader;
@@ -17,17 +18,18 @@ import java.util.List;
  * */
 public class Exemplo2 {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
+        List<Product> list = new ArrayList<>();
         String path = "C:\\temp\\in.txt";
 
         try(BufferedReader bufferedReader = new BufferedReader(new FileReader(path))) {
             String line = bufferedReader.readLine();
             while (line != null){
-                list.add(Integer.parseInt(line));
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
                 line = bufferedReader.readLine();
             }
 
-            Integer x = CalculationService.max(list);
+            Product x = CalculationService.max(list);
             System.out.println("Max: ");
             System.out.println(x);
         } catch (IOException e){
