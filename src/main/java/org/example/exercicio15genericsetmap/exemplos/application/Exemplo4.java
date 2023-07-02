acdc3764aa622111d1ddeae2a1564f5b1dc9ca5b
paulorjuniorp <paulorjuniorp@gmail.com>
2023-07-02 19:18:14 -0300

    Exemplo com curingas delimitados

--- src/main/java/org/example/exercicio15genericsetmap/exemplos/application/Exemplo4.java
+++ src/main/java/org/example/exercicio15genericsetmap/exemplos/application/Exemplo4.java
@@ -16,10 +16,20 @@ public class Exemplo4 {
         myShapes.add(new Rectangle(3.0, 2.0));
         myShapes.add(new Circle(2.0));
 
+        List<Circle> myCircles = new ArrayList<>();
+        myCircles.add(new Circle(2.0));
+        myCircles.add(new Circle(3.0));
+
+        List<Rectangle> myRectangles = new ArrayList<>();
+        myRectangles.add(new Rectangle(2.0, 3.0));
+        myRectangles.add(new Rectangle(5.0, 4.0));
+
+        System.out.println("Total area circles: " + toralArea(myCircles));
+        System.out.println("Total area rectangles: " + toralArea(myRectangles));
         System.out.println("Total area: " + toralArea(myShapes));
     }
 
-    public static double toralArea(List<Shape> list){
+    public static double toralArea(List<? extends Shape> list){
         double sum = 0.0;
         for (Shape s : list){
             sum += s.area();
