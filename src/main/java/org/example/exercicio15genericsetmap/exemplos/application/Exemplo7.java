f13e188db036ef953cd62397580bbd5ae5df2f76
paulorjuniorp <paulorjuniorp@gmail.com>
2023-07-06 21:08:55 -0300

    Exemplo com Set

--- src/main/java/org/example/exercicio15genericsetmap/exemplos/application/Exemplo7.java
+++ src/main/java/org/example/exercicio15genericsetmap/exemplos/application/Exemplo7.java
@@ -1,9 +1,32 @@
 package org.example.exercicio15genericsetmap.exemplos.application;
+
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Set exemplo
+ * HashSet mais rápido porém não ordenado
+ * TreeSet mais lento e ordenado pelo compareTo
+ * LinkedHashSet mais lento que o hash, mas mantém a posição
+ *
+ * métodos: add, remove e contains baseados em hashcode se não existir é utilizado os ponteiros
+ * clear()
+ * size()
+ * removeIf()
  * */
 public class Exemplo7 {
     public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+
+        set.add("TV");
+        set.add("Notebook");
+        set.add("Tablet");
+
+        System.out.println(set.contains("Notebook"));
+
+        for (String s : set) {
+            System.out.println(s);
+        }
 
     }
 }
