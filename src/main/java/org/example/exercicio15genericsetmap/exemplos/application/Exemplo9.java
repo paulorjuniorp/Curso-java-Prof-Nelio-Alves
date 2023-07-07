b0daac4c17ac50a1656d699dcd6046f8f5784b28
paulorjuniorp <paulorjuniorp@gmail.com>
2023-07-07 07:07:48 -0300

    Exemplo de como o Set testa igualdade

--- src/main/java/org/example/exercicio15genericsetmap/exemplos/application/Exemplo9.java
+++ src/main/java/org/example/exercicio15genericsetmap/exemplos/application/Exemplo9.java
@@ -1,5 +1,19 @@
 package org.example.exercicio15genericsetmap.exemplos.application;
 
-public class Exemplo9 {
+import org.example.exercicio15genericsetmap.exemplos.model.entities.Produce;
 
+import java.util.HashSet;
+import java.util.Set;
+
+public class Exemplo9 {
+    public static void main(String[] args) {
+        Set<Produce> set = new HashSet<>();
+        set.add(new Produce("TV",900.0));
+        set.add(new Produce("Notebook", 1200.0));
+        set.add(new Produce("Tablet",400.0));
+
+        Produce prod = new Produce("Notebook",1200.0);
+
+        System.out.println(set.contains(prod));
+    }
 }
