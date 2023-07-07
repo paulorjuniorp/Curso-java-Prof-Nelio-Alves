ab4f75b33f52e55317c901f331266b8c3ef1db93
paulorjuniorp <paulorjuniorp@gmail.com>
2023-07-07 07:10:28 -0300

    Exemplo de como o Set testa igualdade

--- src/main/java/org/example/exercicio15genericsetmap/exemplos/model/entities/Produce.java
+++ src/main/java/org/example/exercicio15genericsetmap/exemplos/model/entities/Produce.java
@@ -1,5 +1,7 @@
 package org.example.exercicio15genericsetmap.exemplos.model.entities;
 
+import java.util.Objects;
+
 public class Produce {
     private String name;
     private Double price;
@@ -23,5 +25,18 @@ public class Produce {
 
     public void setPrice(Double price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produce produce = (Produce) o;
+        return Objects.equals(name, produce.name) && Objects.equals(price, produce.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
     }
 }
