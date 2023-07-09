b75e1f633b85e090043d99323c83829fee875d93
paulorjuniorp <paulorjuniorp@gmail.com>
2023-07-09 17:53:29 -0300

    Exercicio com Set

---
+++ src/main/java/org/example/exercicio15genericsetmap/exercicios/application/Exercicio2.java
@@ -0,0 +1,13 @@
+package org.example.exercicio15genericsetmap.exercicios.application;
+
+/**
+ * Em um portal de cursos online, cada usuário possui um código único, representado por um número inteiro.
+ * Cada instrutor do portal pode ter vários cursos, sendo que um mesmo aluno pode matricular em quantos cursos
+ * quiser. Assim, o número total de alunos de um instrutor não é simplesmente a soma dos alunos de todos os
+ * cursos que ele possui, pois pode haver alunos repetidos em mais de um curso.
+ * O instrutor Alex possui três cursos A, B e C, e deseja saber seu número total de alunos.
+ * Seu programa deve ler os alunos dos cursos A, B e C do instrutor Alex, depois mostrar a quantidade total de
+ * alunos dele, conforme exemplo
+ * */
+public class Exercicio2 {
+}
