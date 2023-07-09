f258d7e719a7366c5f650d1a377691335103d615
paulorjuniorp <paulorjuniorp@gmail.com>
2023-07-09 20:31:00 -0300

    Exercicio com Set

--- src/main/java/org/example/exercicio15genericsetmap/exercicios/application/Exercicio2.java
+++ src/main/java/org/example/exercicio15genericsetmap/exercicios/application/Exercicio2.java
@@ -1,5 +1,9 @@
 package org.example.exercicio15genericsetmap.exercicios.application;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 /**
  * Em um portal de cursos online, cada usuário possui um código único, representado por um número inteiro.
  * Cada instrutor do portal pode ter vários cursos, sendo que um mesmo aluno pode matricular em quantos cursos
@@ -10,4 +14,41 @@ package org.example.exercicio15genericsetmap.exercicios.application;
  * alunos dele, conforme exemplo
  * */
 public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<Integer> cursoA = new HashSet<>();
+        Set<Integer> cursoB = new HashSet<>();
+        Set<Integer> cursoC = new HashSet<>();
+
+        System.out.print("Quantos estudantes matriculados no curso A? ");
+        int entrada = scanner.nextInt();
+        for (int i = 0; i < entrada; i++){
+            int numeroUsuario = scanner.nextInt();
+            cursoA.add(numeroUsuario);
+        }
+
+        System.out.print("Quantos estudantes matriculados no curso B? ");
+        entrada = scanner.nextInt();
+        for (int i = 0; i < entrada; i++){
+            int numeroUsuario = scanner.nextInt();
+            cursoB.add(numeroUsuario);
+        }
+
+        System.out.print("Quantos estudantes matriculados no curso C? ");
+        entrada = scanner.nextInt();
+        for (int i = 0; i < entrada; i++){
+            int numeroUsuario = scanner.nextInt();
+            cursoC.add(numeroUsuario);
+        }
+
+        Set<Integer> totalAlunos = new HashSet<>();
+        totalAlunos.addAll(cursoA);
+        totalAlunos.addAll(cursoB);
+        totalAlunos.addAll(cursoC);
+
+        System.out.println("Total de estudantes: " + totalAlunos.size());
+
+        scanner.close();
+    }
 }
