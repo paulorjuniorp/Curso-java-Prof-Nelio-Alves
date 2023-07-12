c00c60d75207aecbaa6f6587a139f19be6e07089
paulorjuniorp <paulorjuniorp@gmail.com>
2023-07-12 07:20:20 -0300

    Exemplo utilizando comparator do Collections

---
+++ src/main/java/org/example/exercicio16programacaofuncional/exemplos/application/Exemplo1.java
@@ -0,0 +1,17 @@
+package org.example.exercicio16programacaofuncional.exemplos.application;
+
+/**
+ * Problema
+ *
+ * Suponha uma classe Product com os atributos name e price.
+ *
+ * Podemos implementar a comparação de produtos por meio da implementação da interface Comparable<Product>.
+ *
+ * Entretanto, desta forma nossa classe não fica fechada para alteração: se o critério de comparação mudar, precisaremos
+ * alterar a classe Product.
+ *
+ * Podemos então usar o default method "sort" da interface List:
+ * default void sort(Comparator<? super E> c)
+ * */
+public class Exemplo1 {
+}
