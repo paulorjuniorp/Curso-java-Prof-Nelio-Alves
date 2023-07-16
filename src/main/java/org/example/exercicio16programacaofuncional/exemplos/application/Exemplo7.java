688e0c1f4460f1ddb288a3f31e111cb92f020143
paulorjuniorp <paulorjuniorp@gmail.com>
2023-07-15 21:12:48 -0300

    Exemplo com interface predicate

--- src/main/java/org/example/exercicio16programacaofuncional/exemplos/application/Exemplo7.java
+++ src/main/java/org/example/exercicio16programacaofuncional/exemplos/application/Exemplo7.java
@@ -1,10 +1,30 @@
 package org.example.exercicio16programacaofuncional.exemplos.application;
 
+import org.example.exercicio16programacaofuncional.exemplos.model.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Predicate
+ *
+ * Fazer um programa que, a partir de uma lista de produtos remova da lista somente aqueles cujo preço mínimo seja 100.
+ *
  * */
 public class Exemplo7 {
     public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.0));
+        list.add(new Product("Mouse", 50.0));
+        list.add(new Product("Tablet",350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        list.removeIf(p -> p.getPrice() >= 100);
+
+        for (Product product : list) {
+            System.out.println(product);
+        }
 
     }
 }
