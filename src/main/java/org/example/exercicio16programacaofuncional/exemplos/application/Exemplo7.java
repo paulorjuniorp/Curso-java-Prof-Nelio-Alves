4f6532dee536617196a4236ff7899514f669ed57
paulorjuniorp <paulorjuniorp@gmail.com>
2023-07-15 21:08:29 -0300

    Exemplo com interface predicate

---
+++ src/main/java/org/example/exercicio16programacaofuncional/exemplos/application/Exemplo7.java
@@ -0,0 +1,10 @@
+package org.example.exercicio16programacaofuncional.exemplos.application;
+
+/**
+ * Predicate
+ * */
+public class Exemplo7 {
+    public static void main(String[] args) {
+
+    }
+}
