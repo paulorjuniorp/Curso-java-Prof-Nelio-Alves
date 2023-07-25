185daf7cf8727a748e14b79105e4487bfcdbaebb
paulorjuniorp <paulorjuniorp@gmail.com>
2023-07-25 07:18:14 -0300

    Exemplo com acesso ao banco de dados com jdbc: método de encerramento da conexão e criação completa da classe de conexão ao banco de dados

--- src/main/java/org/example/exercicio17jdbc/exemplos/application/Exemplo1.java
+++ src/main/java/org/example/exercicio17jdbc/exemplos/application/Exemplo1.java
@@ -1,12 +1,32 @@
 package org.example.exercicio17jdbc.exemplos.application;
 
 import org.example.exercicio17jdbc.db.DB;
+import org.example.exercicio17jdbc.db.DbException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Exemplo1 {
     public static void main(String[] args) {
-        Connection connection = DB.getConnection();
-        DB.closeConnection();
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DB.getConnection();
+
+            statement = connection.createStatement();
+
+            resultSet = statement.executeQuery("select * from department");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("Id") + ", " + resultSet.getString("Name"));
+            }
+
+        } catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }
     }
 }
