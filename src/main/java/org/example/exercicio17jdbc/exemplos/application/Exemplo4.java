e580a4cdf74e9fb6ef2072927e5b800da8e0b0eb
paulorjuniorp <paulorjuniorp@gmail.com>
2023-07-27 06:49:10 -0300

    Exemplo com acesso ao banco de dados com jdbc: fazendo um delete

--- src/main/java/org/example/exercicio17jdbc/exemplos/application/Exemplo4.java
+++ src/main/java/org/example/exercicio17jdbc/exemplos/application/Exemplo4.java
@@ -1,7 +1,30 @@
 package org.example.exercicio17jdbc.exemplos.application;
 
+import org.example.exercicio17jdbc.db.DB;
+import org.example.exercicio17jdbc.db.DbIntegrityException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Exemplo4 {
     public static void main(String[] args) {
-        
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = DB.getConnection();
+            preparedStatement = connection.prepareStatement("DELETE FROM department WHERE id = ?");
+            preparedStatement.setInt(1,5);
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            System.out.println("DONE! Rows affected: " + rowsAffected);
+
+        } catch (SQLException e) {
+            throw new DbIntegrityException(e.getMessage());
+        } finally {
+            DB.closeStatement(preparedStatement);
+            DB.closeConnection();
+        }
     }
 }
