336985be87610f442b55e012990ac988913beb8a
paulorjuniorp <paulorjuniorp@gmail.com>
2023-08-01 06:43:37 -0300

    Exemplo com jpa criação da estrutura

---
+++ src/main/java/org/example/exercicio18jpa/dominio/Pessoa.java
@@ -0,0 +1,66 @@
+package org.example.exercicio18jpa.dominio;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Pessoa implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String nome;
+    private String email;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(Integer id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return Objects.equals(id, pessoa.id) && Objects.equals(nome, pessoa.nome) && Objects.equals(email, pessoa.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, email);
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
